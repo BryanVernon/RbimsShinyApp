af02b616e0d818267b890783376cb8d43a54c6ce
BryanVernon <bryan.vernon@utexas.edu>
2022-03-07 14:26:02 -0600

    Added to upload data

--- R/RbiMs Application.R
+++ R/RbiMs Application.R
@@ -2,19 +2,62 @@ library(shiny)
 library(markdown)
 
 # Define UI for application that draws a histogram
-ui <- navbarPage(
-  "RbiMs",
-  tabPanel("Home", includeHTML("../Rmd/Home.html")),
-  tabPanel("About",includeHTML("../Rmd/About.html")),
-  tabPanel("Upload data",includeHTML("../Rmd/Upload-data.html")),
-  tabPanel("Contact Us",includeHTML("../Rmd/Contact-Us.html"))
+ui <- fluidPage(
+  navbarPage("RbiMs",
+             tabPanel("About",includeHTML("../Rmd/About.html")),
+             tabPanel("KEGG",
+                      fluidRow(
+                        titlePanel("Upload data"),
+                        sidebarLayout(
+                          sidebarPanel(
+                            fileInput("file1", "Choose your CSV File from KofamKOALA",
+                                      accept = c(
+                                        "text/csv",
+                                        "text/comma-separated-values,text/plain",
+                                        ".csv")
+                                      ),
+                            ),
+                        mainPanel( plotOutput("KEGG"),
+                                   h4("My data", align = "left"),
+                                   tableOutput("contents"))
+                        )
+                      )
+             ),
+             tabPanel("Contact Us",includeHTML("../Rmd/Contact-Us.html")),
+             tabPanel("FAQ", includeHTML("../Rmd/FAQ.html"))
   )
-  
+)
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
 
+
+server <- function(input, output, session) {
+  library(rbims)
+  library(dplyr)
+  library(tidyverse)
+  library(ggplot2)
+  library(shiny)
+  filedata <- reactive({
+    infile <- input$file1
+    if (is.null(infile)){
+      return(NULL)      
+    }
+    read.csv(infile$datapath)
+  })
+  output$contents = renderTable({
+    df <- filedata()
+    return(df)
+  })
+  output$KEGG <- renderUI({
+    df <- filedata()
+    if (is.null(df)) return(NULL)})
+  #KEGG <- read_ko(data_kofam =  df)})
 }
 
+shinyApp(ui, server)
+# Define server logic required to draw a histogram
+#server <- function(input, output) {
+
+#}
+
 # Run the application 
-shinyApp(ui = ui, server = server)
+#shinyApp(ui = ui, server = server)
